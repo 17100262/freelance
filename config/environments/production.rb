72178a8953ef8c1a9254c67af78ed42e27d83564
a17100262 <maueezahmed@gmail.com>
2018-05-04 18:14:34 +0000

    added url for websocket in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -93,6 +93,9 @@ Rails.application.configure do
   
   
   config.action_mailer.default_url_options = { host: 'https://freelance-staging.herokuapp.com' }
+  
+  config.action_cable.url = 'wss://freelance-staging.herokuapp.com/cable'
+  config.action_cable.allowed_request_origins = [ 'http://freelance-staging.herokuapp.com', 'https://freelance-staging.herokuapp.com']
   # config.action_mailer.delivery_method = :sendmail
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = true
