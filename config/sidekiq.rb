7cf03e893fd20c3c55f8687a0ce5b175e235907a
a17100262 <maueezahmed@gmail.com>
2018-06-18 10:30:12 +0000

    modifications

--- config/sidekiq.rb
+++ config/sidekiq.rb
@@ -9,11 +9,11 @@ if Rails.env.development?
   end
 else
   Sidekiq.configure_server do |config|
-    config.redis = { url: 'redis://rediscloud:vFez1P0zXQvtimonZsdzQHkPdHr1f67v@redis-10941.c11.us-east-1-2.ec2.cloud.redislabs.com:10941' }
+    config.redis = { url: ENV['REDISCLOUD_URL'] }
   end
   
   Sidekiq.configure_client do |config|
-    config.redis = { url: 'redis://rediscloud:vFez1P0zXQvtimonZsdzQHkPdHr1f67v@redis-10941.c11.us-east-1-2.ec2.cloud.redislabs.com:10941' }
+    config.redis = { url: ENV['REDISCLOUD_URL'] }
   end
   
 end
