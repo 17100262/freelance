60d325e42c10b87e2acb05c2dc355ca30c23970d
a17100262 <maueezahmed@gmail.com>
2018-06-18 10:34:00 +0000

    modifications

--- config/sidekiq.rb
+++ config/sidekiq.rb
@@ -1,19 +1,8 @@
-if Rails.env.development?
-  
-  Sidekiq.configure_server do |config|
-    config.redis = { url: 'redis://localhost:6379/0' }
-  end
-  
-  Sidekiq.configure_client do |config|
-    config.redis = { url: 'redis://localhost:6379/0'}
-  end
-else
-  Sidekiq.configure_server do |config|
-    config.redis = { url: ENV['REDISCLOUD_URL'] }
-  end
-  
-  Sidekiq.configure_client do |config|
-    config.redis = { url: ENV['REDISCLOUD_URL'] }
-  end
-  
+redis_url = ENV['REDISCLOUD_URL'] || 'redis://localhost:6379'
+Sidekiq.configure_server do |config|
+  config.redis = { url: redis_url  }
+end
+
+Sidekiq.configure_client do |config|
+  config.redis = { url: redis_url}
 end
